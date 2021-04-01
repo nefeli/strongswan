142ec021ca0a4646f137aaf41550d1a747ada398
Melvin Walls <mwalls67@gmail.com>
2021-04-01 11:14:01 -0700

    XXX sa_notify: encode messages as protobufs

--- src/libcharon/plugins/sa_notify/sa_notify_listener.c
+++ src/libcharon/plugins/sa_notify/sa_notify_listener.c
@@ -23,8 +23,10 @@
 #define _GNU_SOURCE
 #include <inttypes.h>
 #include <stdio.h>
+#include <netinet/in.h>
 
 #include "sa_notify_listener.h"
+#include "ipsec_msg.pb-c.h"
 
 #include <daemon.h>
 
@@ -160,46 +162,195 @@ static inline void esp_names(proposal_t *proposal, const char **enc,
 	*integ = algo_name(esp_integ, countof(esp_integ), alg, len);
 }
 
-void print_selectors(int family, bool local, enumerator_t *enumerator, FILE *f)
-{
-	char *field = local ? "LOCAL" : "REMOTE";
-
-	traffic_selector_t *ts;
-	while (enumerator->enumerate(enumerator, &ts))
-	{
-		/* For ports, "If the protocol is ICMP/ICMPv6 the ICMP type and code are stored in this
-		 * field as follows:  The message type is placed in the most significant
-		 * 8 bits and the code in the least significant 8 bits". */
-		fprintf(f,
-			",\"SELECTOR\",\"%s\",\"%N\",%u,\"%H\",\"%H\",%u,%u", field, ts_type_name,
-			ts->get_type(ts), ts->get_protocol(ts),
-			host_create_from_chunk(family, ts->get_from_address(ts), ts->get_from_port(ts)),
-			host_create_from_chunk(family, ts->get_to_address(ts), ts->get_to_port(ts)),
-			ts->get_from_port(ts), ts->get_to_port(ts));
-	}
-}
-
-void print_sa(uint32_t uid, char *family_str, int family, host_t *local,
+static void print_sa(uint32_t uid, int family, host_t *local,
 				host_t *remote, protocol_id_t protocol, ipsec_mode_t mode,
-				uint32_t spi, const char *enc, const chunk_t *encr_key,
-				const char *integ, const chunk_t *integ_key, time_t lifetime,
-				child_sa_t *child_sa, char *ike_name, FILE *f)
+				uint32_t spi, proposal_t *proposal, const chunk_t *encr_key,
+				child_sa_t *child_sa, const char *ike_name, FILE *f)
 {
-	/* Print SA */
-	fprintf(f,
-		"\"CREATE\",%u,\"%s\",\"%s\",\"%N\",\"%N\",\"%s\",\"%H\",\"%H\",\"0x%.8x\","
-		"\"%s\",\"0x%+B\",\"%s\",\"0x%+B\",%ld",
-		 uid, ike_name, child_sa->get_name(child_sa), protocol_id_names,
-        protocol, ipsec_mode_names, mode, family_str, local, remote, spi, enc,
-        encr_key, integ, integ_key, lifetime);
+  Nefeli__Pb__SA sa_pb = NEFELI__PB__SA__INIT;
+  sa_pb.has_local = 1;
+  sa_pb.local = ntohl(((struct sockaddr_in*)local->get_sockaddr(local))->sin_addr.s_addr);
 
-	/* Print local */
-	print_selectors(family, TRUE, child_sa->create_ts_enumerator(child_sa, TRUE), f);
+  sa_pb.has_remote = 1;
+  sa_pb.remote = ntohl(((struct sockaddr_in*)remote->get_sockaddr(remote))->sin_addr.s_addr);
 
-	/* Print remote */
-	print_selectors(family, FALSE, child_sa->create_ts_enumerator(child_sa, FALSE), f);
+  sa_pb.has_spi = 1;
+  sa_pb.spi = ntohl(spi);
 
-	fprintf(f, "\n");
+  sa_pb.has_encr_key = 1;
+  sa_pb.encr_key.data = malloc(encr_key->len);
+  sa_pb.encr_key.len = encr_key->len;
+  memcpy(sa_pb.encr_key.data, encr_key->ptr, encr_key->len);
+
+	uint16_t alg, key_size;
+	if (proposal->get_algorithm(proposal, ENCRYPTION_ALGORITHM, &alg, &key_size)) {
+    switch (alg) {
+    case ENCR_NULL:
+      sa_pb.has_encr_alg = 1;
+      sa_pb.encr_alg = NEFELI__PB__ENCRYPTION_ALGORITHM__NULL_ENCR;
+      break;
+    case ENCR_AES_GCM_ICV8:
+    case ENCR_AES_GCM_ICV12:
+    case ENCR_AES_GCM_ICV16:
+      sa_pb.has_encr_alg = 1;
+      sa_pb.encr_alg = NEFELI__PB__ENCRYPTION_ALGORITHM__AES_GCM;
+      break;
+    }
+  }
+
+  sa_pb.has_tunnel = 1;
+  sa_pb.tunnel = mode == MODE_TUNNEL;
+
+  traffic_selector_t *ts;
+  enumerator_t *local_selectors = child_sa->create_ts_enumerator(child_sa, TRUE);
+  while (local_selectors->enumerate(local_selectors, &ts)) {
+    sa_pb.n_selectors++;
+  }
+
+  enumerator_t *remote_selectors = child_sa->create_ts_enumerator(child_sa, FALSE);
+  while (remote_selectors->enumerate(remote_selectors, &ts)) {
+    sa_pb.n_selectors++;
+  }
+  Nefeli__Pb__Selector *pb_selectors[32];
+  sa_pb.selectors = &pb_selectors;
+  for (size_t i = 0; i < sa_pb.n_selectors; i++) {
+    pb_selectors[i] = malloc(sizeof(Nefeli__Pb__Selector));
+    nefeli__pb__selector__init(pb_selectors[i]);
+  }
+
+  size_t i = 0;
+  char lbuf[1024];
+  local_selectors = child_sa->create_ts_enumerator(child_sa, TRUE);
+  while(local_selectors->enumerate(local_selectors, &ts)) {
+    Nefeli__Pb__Selector *sel_pb = sa_pb.selectors[i];
+
+    host_t *host = host_create_from_chunk(family, ts->get_from_address(ts), 0);
+    uint32_t start_addr = ntohl(((struct sockaddr_in*)host->get_sockaddr(host))->sin_addr.s_addr);
+    host = host_create_from_chunk(family, ts->get_to_address(ts), 0);
+    uint32_t end_addr = ntohl(((struct sockaddr_in*)host->get_sockaddr(host))->sin_addr.s_addr);
+
+    sel_pb->local_addrs = malloc(sizeof(Nefeli__Pb__Selector__Address));
+    nefeli__pb__selector__address__init(sel_pb->local_addrs);
+
+    if (start_addr == 0 && end_addr == 0xffffffff) {
+      sel_pb->local_addrs->value_case = NEFELI__PB__SELECTOR__ADDRESS__VALUE_ANY;
+      sel_pb->local_addrs->any = 1;
+    } else {
+      int bytes_out = snprintf(lbuf, 1024, "%u,%u", start_addr,end_addr);
+      sel_pb->local_addrs->value_case = NEFELI__PB__SELECTOR__ADDRESS__VALUE_LITERAL;
+      sel_pb->local_addrs->literal = malloc(bytes_out + 1);
+      memcpy(sel_pb->local_addrs->literal, lbuf, bytes_out);
+    }
+
+    uint8_t proto = ts->get_protocol(ts);
+    sel_pb->proto = malloc(sizeof(Nefeli__Pb__Selector__Protocol));
+    nefeli__pb__selector__protocol__init(sel_pb->proto);
+    if (proto == 0) {
+      sel_pb->proto->value_case = NEFELI__PB__SELECTOR__PROTOCOL__VALUE_ANY;
+      sel_pb->proto->any = 1;
+    } else {
+      sel_pb->proto->value_case = NEFELI__PB__SELECTOR__PROTOCOL__VALUE_LITERAL;
+      sel_pb->proto->literal = proto;
+    }
+
+    if (proto == 0 || proto == IPPROTO_UDP || proto == IPPROTO_TCP) {
+      uint16_t start_port = ts->get_from_port(ts);
+      uint16_t end_port = ts->get_to_port(ts);
+
+      sel_pb->local_ports = malloc(sizeof(Nefeli__Pb__Selector__Port));
+      nefeli__pb__selector__port__init(sel_pb->local_ports);
+      if (start_port == 0 && end_port == 0xffff) {
+        sel_pb->local_ports->value_case = NEFELI__PB__SELECTOR__PORT__VALUE_ANY;
+        sel_pb->local_ports->any = 1;
+      } else if (start_port == 0xffff && end_port == 0) {
+        sel_pb->local_ports->value_case = NEFELI__PB__SELECTOR__PORT__VALUE_OPAQUE;
+        sel_pb->local_ports->opaque = 1;
+      } else {
+        sel_pb->local_ports->value_case = NEFELI__PB__SELECTOR__PORT__VALUE_LITERAL;
+        sel_pb->local_ports->literal = malloc(sizeof(Nefeli__Pb__Selector__Port__Range));
+        nefeli__pb__selector__port__range__init(sel_pb->local_ports->literal);
+        sel_pb->local_ports->literal->has_start = 1;
+        sel_pb->local_ports->literal->start = start_port;
+        sel_pb->local_ports->literal->has_end = 1;
+        sel_pb->local_ports->literal->end = end_port;
+      }
+    }
+    i++;
+  }
+
+  remote_selectors = child_sa->create_ts_enumerator(child_sa, FALSE);
+  while(remote_selectors->enumerate(remote_selectors, &ts)) {
+    Nefeli__Pb__Selector *sel_pb = sa_pb.selectors[i];
+
+    host_t *host = host_create_from_chunk(family, ts->get_from_address(ts), 0);
+    uint32_t start_addr = ntohl(((struct sockaddr_in*)host->get_sockaddr(host))->sin_addr.s_addr);
+    host = host_create_from_chunk(family, ts->get_to_address(ts), 0);
+    uint32_t end_addr = ntohl(((struct sockaddr_in*)host->get_sockaddr(host))->sin_addr.s_addr);
+
+    sel_pb->remote_addrs = malloc(sizeof(Nefeli__Pb__Selector__Address));
+    nefeli__pb__selector__address__init(sel_pb->remote_addrs);
+
+    if (start_addr == 0 && end_addr == 0xffffffff) {
+      sel_pb->remote_addrs->value_case = NEFELI__PB__SELECTOR__ADDRESS__VALUE_ANY;
+      sel_pb->remote_addrs->any = 1;
+    } else {
+      int bytes_out = snprintf(lbuf, 1024, "%u,%u", start_addr,end_addr);
+      sel_pb->remote_addrs->value_case = NEFELI__PB__SELECTOR__ADDRESS__VALUE_LITERAL;
+      sel_pb->remote_addrs->literal = malloc(bytes_out + 1);
+      memcpy(sel_pb->remote_addrs->literal, lbuf, bytes_out);
+    }
+
+    uint8_t proto = ts->get_protocol(ts);
+    sel_pb->proto = malloc(sizeof(Nefeli__Pb__Selector__Protocol));
+    nefeli__pb__selector__protocol__init(sel_pb->proto);
+    if (proto == 0) {
+      sel_pb->proto->value_case = NEFELI__PB__SELECTOR__PROTOCOL__VALUE_ANY;
+      sel_pb->proto->any = 1;
+    } else {
+      sel_pb->proto->value_case = NEFELI__PB__SELECTOR__PROTOCOL__VALUE_LITERAL;
+      sel_pb->proto->literal = proto;
+    }
+
+    if (proto == 0 || proto == IPPROTO_UDP || proto == IPPROTO_TCP) {
+      uint16_t start_port = ts->get_from_port(ts);
+      uint16_t end_port = ts->get_to_port(ts);
+
+      sel_pb->remote_ports = malloc(sizeof(Nefeli__Pb__Selector__Port));
+      nefeli__pb__selector__port__init(sel_pb->remote_ports);
+      if (start_port == 0 && end_port == 0xffff) {
+        sel_pb->remote_ports->value_case = NEFELI__PB__SELECTOR__PORT__VALUE_ANY;
+        sel_pb->remote_ports->any = 1;
+      } else if (start_port == 0xffff && end_port == 0) {
+        sel_pb->remote_ports->value_case = NEFELI__PB__SELECTOR__PORT__VALUE_OPAQUE;
+        sel_pb->remote_ports->opaque = 1;
+      } else {
+        sel_pb->remote_ports->value_case = NEFELI__PB__SELECTOR__PORT__VALUE_LITERAL;
+        sel_pb->remote_ports->literal = malloc(sizeof(Nefeli__Pb__Selector__Port__Range));
+        nefeli__pb__selector__port__range__init(sel_pb->remote_ports->literal);
+        sel_pb->remote_ports->literal->has_start = 1;
+        sel_pb->remote_ports->literal->start = start_port;
+        sel_pb->remote_ports->literal->has_end = 1;
+        sel_pb->remote_ports->literal->end = end_port;
+      }
+    }
+    i++;
+  }
+
+
+  chunk_t buf, buf64;
+  buf.len = nefeli__pb__sa__get_packed_size(&sa_pb);
+  buf.ptr = malloc(buf.len);
+  nefeli__pb__sa__pack(&sa_pb, buf.ptr);
+  buf64 = chunk_to_base64(buf, NULL);
+
+	fprintf(f, "CREATE,%s,%s\n", ike_name, buf64.ptr);
+
+  chunk_free(&buf);
+  chunk_free(&buf64);
+  free(sa_pb.encr_key.data);
+  for (size_t i = 0; i < sa_pb.n_selectors; i++) {
+    nefeli__pb__selector__free_unpacked(sa_pb.selectors[i], NULL);
+  }
 }
 
 /**
@@ -217,58 +368,43 @@ METHOD(listener_t, child_derived_keys, bool, private_sa_notify_listener_t *this,
 
 	host_t *init, *resp;
 	uint32_t spi_i, spi_r;
-	const char *enc = NULL, *integ = NULL;
-	char *family;
 	protocol_id_t protocol = child_sa->get_protocol(child_sa);
 	ipsec_mode_t mode = child_sa->get_mode(child_sa);
-	time_t lifetime = child_sa->get_lifetime(child_sa, TRUE) - child_sa->get_installtime(child_sa);
-	esp_names(child_sa->get_proposal(child_sa), &enc, &integ);
-	if (enc == NULL || integ == NULL)
-	{
-		DBG0(DBG_CHD, "Unsupported algorithm encryption: \"%s\" integrity: \"%s\"",
-			enc, integ);
-	  return TRUE;
-	}
-	uint32_t uid = child_sa->get_unique_id(child_sa);
-	if (enc && integ)
-	{
-		/* Since the IPs are printed this is not compatible with MOBIKE */
-		if (initiator)
-		{
-			init = ike_sa->get_my_host(ike_sa);
-			resp = ike_sa->get_other_host(ike_sa);
-		}
-		else
-		{
-			init = ike_sa->get_other_host(ike_sa);
-			resp = ike_sa->get_my_host(ike_sa);
-		}
-		spi_i = child_sa->get_spi(child_sa, initiator);
-		spi_r = child_sa->get_spi(child_sa, !initiator);
-		family = init->get_family(init) == AF_INET ? "IPv4" : "IPv6";
+	proposal_t *proposal = child_sa->get_proposal(child_sa);
 
-		/* a CHILD_SA consists of a pair of SAs */
-		print_sa(uid, family, init->get_family(init), init, resp,
-			protocol, mode, ntohl(spi_r), enc, &encr_i, integ, &integ_i,
-			lifetime, child_sa, ike_sa->get_name(ike_sa), this->f);
-		print_sa(uid, family, init->get_family(init), resp, init,
-			protocol, mode, ntohl(spi_i), enc, &encr_r, integ, &integ_r,
-            lifetime, child_sa, ike_sa->get_name(ike_sa), this->f);
-		int res = fflush(this->f);
-		if (res != 0)
-		{
-			DBG0(DBG_CHD,
-				"Failed to flush child_derived_keys to file \"%s\" for \"%ld\"",
-				this->filepath, child_sa->get_unique_id(child_sa));
-		}
-	}
-	else
-	{
-		DBG1(DBG_CHD,
-			"CHILD_SA %d has %sencrption alg and has %sintegrity alg "
-			"but requires both",
-			uid, enc ? "" : "no ", integ ? "" : "no ");
-	}
+	uint32_t uid = child_sa->get_unique_id(child_sa);
+  /* Since the IPs are printed this is not compatible with MOBIKE */
+  if (initiator)
+  {
+    init = ike_sa->get_my_host(ike_sa);
+    resp = ike_sa->get_other_host(ike_sa);
+  }
+  else
+  {
+    init = ike_sa->get_other_host(ike_sa);
+    resp = ike_sa->get_my_host(ike_sa);
+  }
+  spi_i = child_sa->get_spi(child_sa, initiator);
+  spi_r = child_sa->get_spi(child_sa, !initiator);
+
+  /* a CHILD_SA consists of a pair of SAs */
+  print_sa(
+    uid, init->get_family(init), init, resp,
+    protocol, mode, spi_r, proposal, &encr_i, child_sa,
+    ike_sa->get_name(ike_sa), this->f);
+
+  print_sa(
+    uid, init->get_family(init), resp, init,
+    protocol, mode, spi_i, proposal, &encr_r, child_sa,
+    ike_sa->get_name(ike_sa), this->f);
+  int res = fflush(this->f);
+  if (res != 0)
+  {
+    DBG0(DBG_CHD,
+      "Failed to flush child_derived_keys to file \"%s\" for \"%ld\"",
+      this->filepath, child_sa->get_unique_id(child_sa));
+  }
+
 	return TRUE;
 }
 
